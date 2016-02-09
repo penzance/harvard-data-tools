7e2f60f13115c38ee4631d767c52d1a0f5a52066
Kiczek, Ethan <ethan_kiczek@harvard.edu>
2016-02-09 16:33:41 -0500

    Fixing `bootstrap.sh` to use /home/hadoop instead of /root, also sudo on the yum install.

--- emr/bootstrap.sh
+++ emr/bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # add github.com to known_hosts and run ansible-pull
-ssh-keyscan github.com >> /root/.ssh/known_hosts
+ssh-keyscan github.com >> /home/hadoop/.ssh/known_hosts
 
 # install jdk and git
-yum install -y java-devel git-core
+sudo yum install -y java-devel git-core
 
 # clone our repo
-git clone -b master https://github.com/penzance/harvard-data-tools.git /root/harvard-data-tools
+git clone -b master https://github.com/penzance/harvard-data-tools.git /home/hadoop/harvard-data-tools
