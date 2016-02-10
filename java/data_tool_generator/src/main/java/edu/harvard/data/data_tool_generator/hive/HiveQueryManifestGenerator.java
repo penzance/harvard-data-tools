c18bb86f4dae99c7632a1af84ffcb6a6d5ed6179
Phil McGachey <phil_mcgachey@harvard.edu>
2016-02-10 14:39:18 -0500

    Added logging to Hive manifest generator

--- java/data_tool_generator/src/main/java/edu/harvard/data/data_tool_generator/hive/HiveQueryManifestGenerator.java
+++ java/data_tool_generator/src/main/java/edu/harvard/data/data_tool_generator/hive/HiveQueryManifestGenerator.java
@@ -5,11 +5,16 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import edu.harvard.data.data_tool_generator.CanvasDataGenerator;
 import edu.harvard.data.data_tool_generator.SchemaPhase;
 import edu.harvard.data.data_tool_generator.SchemaTransformer;
 
 public class HiveQueryManifestGenerator {
+  private static final Logger log = LogManager.getLogger();
+
   private final File gitDir;
   private final SchemaTransformer schemaVersions;
   private final File dir;
@@ -25,6 +30,10 @@ public class HiveQueryManifestGenerator {
     final File phase1File = new File(dir, "canvas_phase_1_hive.q");
     final File phase2File = new File(dir, "canvas_phase_2_hive.q");
 
+    log.info("Phase 1 file: "+ phase1File);
+    log.info("Phase 2 file: "+ phase2File);
+    log.info("Phase 1 hive directory: "+ new File(gitDir, "hive/phase_1"));
+    log.info("Phase 2 hive directory: "+ new File(gitDir, "hive/phase_2"));
     try (final PrintStream out = new PrintStream(new FileOutputStream(phase1File))) {
       generateHiveManifest(out, schemaVersions.getPhase(1), new File(gitDir, "hive/phase_1"));
     }
