271cd974d405b137406e3de3630b7071768be364
Phil McGachey <phil_mcgachey@harvard.edu>
2016-02-23 17:01:01 -0500

    Added debug logging to Hadoop runner

--- java/data_tools/src/main/java/edu/harvard/data/data_tools/canvas/CanvasHadoopCommand.java
+++ java/data_tools/src/main/java/edu/harvard/data/data_tools/canvas/CanvasHadoopCommand.java
@@ -8,8 +8,9 @@ import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.kohsuke.args4j.Argument;
-import org.mortbay.log.Log;
 
 import edu.harvard.data.client.AwsUtils;
 import edu.harvard.data.client.DataConfiguration;
@@ -23,6 +24,8 @@ import edu.harvard.data.data_tools.canvas.phase1.RequestPerFileJob;
 import edu.harvard.data.data_tools.canvas.phase1.RequestPerPageJob;
 
 public class CanvasHadoopCommand implements Command {
+
+  private static final Logger log = LogManager.getLogger();
 
   @Argument(index = 0, usage = "Hadoop process phase.", metaVar = "0", required = true)
   public int phase;
@@ -45,6 +48,11 @@ public class CanvasHadoopCommand implements Command {
       throw new DataConfigurationException(e);
     }
 
+    log.info("Setting up Canvas Hadoop job");
+    log.info("Phase: " + phase);
+    log.info("Input directory: " + inputDir);
+    log.info("Output directory: " + outputDir);
+
     final List<Job> jobs = new ArrayList<Job>();
     switch (phase) {
     case 1:
@@ -61,11 +69,12 @@ public class CanvasHadoopCommand implements Command {
     for (final Job job : jobs) {
       job.setJarByClass(CanvasHadoopCommand.class);
       try {
+        log.info("Submitted job " + job.getJobName());
         job.submit();
       } catch (final ClassNotFoundException e) {
         throw new DataConfigurationException(e);
       } catch (final InterruptedException e) {
-        Log.info("Job submission interrupted", e);
+        log.error("Job submission interrupted", e);
       }
     }
     for (final Job job : jobs) {
@@ -73,10 +82,11 @@ public class CanvasHadoopCommand implements Command {
         try {
           Thread.sleep(Job.getCompletionPollInterval(conf));
         } catch (final InterruptedException e) {
-          Log.info("Interrupted while waiting for job to complete.", e);
+          log.error("Interrupted while waiting for job to complete.", e);
         }
       }
     }
+    log.info("All jobs complete");
     return ReturnStatus.OK;
   }
 
