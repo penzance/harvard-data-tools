2d0c6bb96618338c9a19a0a4292ace23af0b257f
Phil McGachey <phil_mcgachey@harvard.edu>
2016-04-07 20:58:51 -0400

    Adding Matterhorn hadoop jobs

--- java/matterhorn_data_tools/src/main/java/edu/harvard/data/matterhorn/phase_2/Phase2HadoopManager.java
+++ java/matterhorn_data_tools/src/main/java/edu/harvard/data/matterhorn/phase_2/Phase2HadoopManager.java
@@ -20,6 +20,8 @@ public class Phase2HadoopManager {
     hadoopConfig.set("format", Format.DecompressedCanvasDataFlatFiles.toString());
     final List<Job> jobs = new ArrayList<Job>();
     jobs.add(new VideoJob(hadoopConfig, aws, hdfsService, inputDir, outputDir).getJob());
+    jobs.add(new SessionJob(hadoopConfig, aws, hdfsService, inputDir, outputDir).getJob());
+    jobs.add(new EventTypeJob(hadoopConfig, aws, hdfsService, inputDir, outputDir).getJob());
     return jobs;
   }
 
