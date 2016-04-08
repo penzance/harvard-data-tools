d166b29c2c562e888c95d6ba25184ecdb1acd066
Phil McGachey <phil_mcgachey@harvard.edu>
2016-04-07 21:31:20 -0400

    Gathering event types

--- java/matterhorn_data_tools/src/main/java/edu/harvard/data/matterhorn/phase_2/EventTypeJob.java
+++ java/matterhorn_data_tools/src/main/java/edu/harvard/data/matterhorn/phase_2/EventTypeJob.java
@@ -63,10 +63,10 @@ class EventTypeMapper extends Mapper<Object, Text, Text, NullWritable> {
   }
 }
 
-class EventTypeReducer extends Reducer<Text, Text, Text, NullWritable> {
+class EventTypeReducer extends Reducer<Text, NullWritable, Text, NullWritable> {
 
   @Override
-  public void reduce(final Text key, final Iterable<Text> values, final Context context)
+  public void reduce(final Text key, final Iterable<NullWritable> values, final Context context)
       throws IOException, InterruptedException {
     context.write(key, NullWritable.get());
   }
