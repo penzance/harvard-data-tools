615cd98b003ef1329dd8f66b482a59de007571ba
Phil McGachey <phil_mcgachey@harvard.edu>
2016-04-12 10:08:14 -0400

    Moved format caching to common phase 2/3 code

--- java/canvas_data_tools/src/main/java/edu/harvard/data/canvas/phase_3/Phase3HadoopManager.java
+++ java/canvas_data_tools/src/main/java/edu/harvard/data/canvas/phase_3/Phase3HadoopManager.java
@@ -11,11 +11,13 @@ import org.apache.hadoop.mapreduce.Job;
 import edu.harvard.data.AwsUtils;
 import edu.harvard.data.DataConfiguration;
 import edu.harvard.data.DataConfigurationException;
+import edu.harvard.data.FormatLibrary.Format;
 
 public class Phase3HadoopManager {
   public static List<Job> getJobs(final AwsUtils aws, final Configuration hadoopConfig,
       final DataConfiguration config, final URI hdfsService, final String inputDir,
       final String outputDir) throws DataConfigurationException, IOException {
+    hadoopConfig.set("format", Format.DecompressedCanvasDataFlatFiles.toString());
     final List<Job> jobs = new ArrayList<Job>();
     jobs.add(new SessionsJob(hadoopConfig, aws, hdfsService, inputDir, outputDir).getJob());
     return jobs;
