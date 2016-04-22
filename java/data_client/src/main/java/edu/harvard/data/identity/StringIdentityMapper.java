9fe4f5a7c1ee4286049f476ef5f84938daaac103
Phil McGachey <phil_mcgachey@harvard.edu>
2016-04-22 08:23:49 -0400

    Unit tests for identity mapper

--- java/data_client/src/main/java/edu/harvard/data/identity/StringIdentityMapper.java
+++ java/data_client/src/main/java/edu/harvard/data/identity/StringIdentityMapper.java
@@ -6,12 +6,9 @@ import java.util.Map;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-import edu.harvard.data.TableFormat;
-
 public abstract class StringIdentityMapper extends
 Mapper<Object, Text, Text, HadoopIdentityKey> implements GeneratedIdentityMapper<String> {
 
-  protected TableFormat format;
   private final IdentityMapper<String> mapper;
 
   public StringIdentityMapper() {
@@ -26,7 +23,7 @@ Mapper<Object, Text, Text, HadoopIdentityKey> implements GeneratedIdentityMapper
   @Override
   public void map(final Object key, final Text value, final Context context)
       throws IOException, InterruptedException {
-    final Map<String, HadoopIdentityKey> map = mapper.map(key, value, context, this);
+    final Map<String, HadoopIdentityKey> map = mapper.map(value, context, this);
     for (final String hadoopKey : map.keySet()) {
       final HadoopIdentityKey identityKey = map.get(hadoopKey);
       context.write(new Text(hadoopKey), identityKey);
