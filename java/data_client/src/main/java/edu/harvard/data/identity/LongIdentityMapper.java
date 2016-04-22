bca26ee0801427da0457b7fdc37a859f39ddc1fe
Phil McGachey <phil_mcgachey@harvard.edu>
2016-04-22 08:23:49 -0400

    Unit tests for identity mapper

--- java/data_client/src/main/java/edu/harvard/data/identity/LongIdentityMapper.java
+++ java/data_client/src/main/java/edu/harvard/data/identity/LongIdentityMapper.java
@@ -7,13 +7,10 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-import edu.harvard.data.TableFormat;
-
 public abstract class LongIdentityMapper extends
 Mapper<Object, Text, LongWritable, HadoopIdentityKey> implements GeneratedIdentityMapper<Long> {
 
-  protected TableFormat format;
-  private final IdentityMapper<Long> mapper;
+  protected final IdentityMapper<Long> mapper;
 
   public LongIdentityMapper() {
     mapper = new IdentityMapper<Long>();
@@ -27,7 +24,7 @@ Mapper<Object, Text, LongWritable, HadoopIdentityKey> implements GeneratedIdenti
   @Override
   public void map(final Object key, final Text value, final Context context)
       throws IOException, InterruptedException {
-    final Map<Long, HadoopIdentityKey> map = mapper.map(key, value, context, this);
+    final Map<Long, HadoopIdentityKey> map = mapper.map(value, context, this);
     for (final Long hadoopKey : map.keySet()) {
       final HadoopIdentityKey identityKey = map.get(hadoopKey);
       context.write(new LongWritable(hadoopKey), identityKey);
