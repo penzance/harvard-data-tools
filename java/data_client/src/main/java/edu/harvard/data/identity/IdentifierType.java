c28035ba3daecafe2e08d546f323fa2ae3f21439
Phil McGachey <phil_mcgachey@harvard.edu>
2016-04-26 15:52:35 -0400

    Fixing up regular expressions for identifier types

--- java/data_client/src/main/java/edu/harvard/data/identity/IdentifierType.java
+++ java/data_client/src/main/java/edu/harvard/data/identity/IdentifierType.java
@@ -3,19 +3,19 @@ package edu.harvard.data.identity;
 import java.util.regex.Pattern;
 
 public enum IdentifierType {
-  ResearchUUID ("research_id", ".*", String.class),
+  ResearchUUID ("research_id", "[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}", String.class),
   HUID ("huid", "\\d{8}", String.class),
   XID ("xid", "[a-zA-Z][\\d|\\w]{7}", String.class),
-  CanvasID ("canvas_id", ".*", Long.class),
-  CanvasDataID ("canvas_data_id", ".*", Long.class),
-  Other ("other", ".*", Void.class);
+  CanvasID ("canvas_id", "", Long.class),
+  CanvasDataID ("canvas_data_id", "", Long.class),
+  Other ("other", "", Void.class);
 
   private String fieldName;
   private Pattern pattern;
   private Class<?> type;
   private IdentifierType(final String fieldName, final String regex, final Class<?> type) {
     this.fieldName = fieldName;
-    pattern = Pattern.compile(regex);
+    pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
     this.type = type;
   }
 
@@ -24,6 +24,9 @@ public enum IdentifierType {
   }
 
   public Pattern getPattern() {
+    if (!type.equals(String.class)) {
+      throw new RuntimeException("Can't access regex pattern for an identifier of type " + type);
+    }
     return pattern;
   }
 
