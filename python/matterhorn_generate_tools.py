fd3e52144d5311374553dbef7eab55220a1c00f4
Phil McGachey <phil_mcgachey@harvard.edu>
2016-06-10 10:14:27 -0400

    Removing pipeline ID dependency from generator

--- python/matterhorn_generate_tools.py
+++ python/matterhorn_generate_tools.py
@@ -16,7 +16,6 @@ GIT_BASE = os.environ['HARVARD_DATA_TOOLS_BASE']
 GENERATED_CODE_DIR = os.environ['HARVARD_DATA_GENERATED_OUTPUT']
 CURRENT_SCHEMA = os.environ['DATA_SCHEMA_VERSION']
 CONFIG_PATHS = os.environ['CONFIG_PATHS']
-PIPELINE_ID = os.environ['PIPELINE_ID']
 
 DATA_CLIENT_DIR = "{0}/java/data_client".format(GIT_BASE)
 DATA_TOOLS_DIR = "{0}/java/matterhorn_data_tools".format(GIT_BASE)
@@ -58,7 +57,6 @@ def run_generator():
         CONFIG_PATHS,
         GIT_BASE,
         GENERATED_CODE_DIR,
-        PIPELINE_ID
     ]
     print "Running {0} in {1}".format(command, GENERATED_CODE_DIR)
     process = subprocess.Popen(command)
