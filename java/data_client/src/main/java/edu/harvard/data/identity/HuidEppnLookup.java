af8b7997656281b21e6f36aefb52955840698857
Phil McGachey <phil_mcgachey@harvard.edu>
2016-08-03 21:27:32 +0000

    Debugging EPPN lookup

--- java/data_client/src/main/java/edu/harvard/data/identity/HuidEppnLookup.java
+++ java/data_client/src/main/java/edu/harvard/data/identity/HuidEppnLookup.java
@@ -30,11 +30,12 @@ public class HuidEppnLookup {
   private void expandIdentities(final List<IdentityMap> identities, final int start, final int end)
       throws DataConfigurationException, SQLException {
     final String view = config.getIdentityOracleSchema() + "." + config.getIdentityOracleView();
-    String queryString = "SELECT * FROM " + view + " WHERE huid IN (";
+    String queryString = "SELECT huid,eppn,adid FROM " + view + " WHERE huid IN (";
     for (int i = start; i < end; i++) {
       queryString += "?, ";
     }
     queryString = queryString.substring(0, queryString.length() - 2) + ")";
+    System.out.println("Query: " + queryString);
 
     final String connectionString = config.getIdentityOracleUrl();
     try {
@@ -49,14 +50,15 @@ public class HuidEppnLookup {
         PreparedStatement statement = connection.prepareStatement(queryString);) {
       for (int i = start; i < end; i++) {
         statement.setString(i - start + 1, (String) identities.get(i).get(IdentifierType.HUID));
+	System.out.println("Set " + (i - start + 1) + ": " + identities.get(i).get(IdentifierType.HUID));
       }
       try (final ResultSet rs = statement.executeQuery();) {
         for (int i = start; i < end; i++) {
           rs.next();
           final IdentityMap id = identities.get(i);
           id.set(IdentifierType.HUID, rs.getString("huid"));
-          id.set(IdentifierType.HUID, rs.getString("eppn"));
-          id.set(IdentifierType.HUID, rs.getString("adid"));
+          id.set(IdentifierType.EPPN, rs.getString("eppn"));
+          id.set(IdentifierType.ADID, rs.getString("adid"));
         }
       }
     }
