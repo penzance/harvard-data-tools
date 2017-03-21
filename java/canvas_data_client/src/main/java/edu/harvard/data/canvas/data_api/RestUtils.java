49e7a7f55dc6bc061c00f1d371ad96a9258bbb86
Phil McGachey <phil_mcgachey@harvard.edu>
2017-03-21 12:55:29 -0400

    Adding debug output to rest utils

--- java/canvas_data_client/src/main/java/edu/harvard/data/canvas/data_api/RestUtils.java
+++ java/canvas_data_client/src/main/java/edu/harvard/data/canvas/data_api/RestUtils.java
@@ -1,8 +1,10 @@
 package edu.harvard.data.canvas.data_api;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.text.DateFormat;
@@ -59,14 +61,22 @@ public class RestUtils {
       if (status != expectedStatus) {
         log.warn("Unexpected REST API response: " + status);
         if (response.getEntity().getContentLength() > 0) {
-          final String responseValue = mapper.readValue(response.getEntity().getContent(),
-              String.class);
-          log.warn(responseValue);
+          final StringBuffer sb = new StringBuffer();
+          try (BufferedReader in = new BufferedReader(
+              new InputStreamReader(response.getEntity().getContent()))) {
+            String line = in.readLine();
+            while (line != null) {
+              sb.append(line);
+              sb.append('\n');
+            }
+            line = in.readLine();
+          }
+          log.warn(sb.toString());
         }
         throw new UnexpectedApiResponseException(expectedStatus, status, url);
       }
       final String responseValue = EntityUtils.toString(response.getEntity());
-      //      System.out.println(responseValue);
+      // System.out.println(responseValue);
       return mapper.readValue(responseValue, type);
     }
   }
