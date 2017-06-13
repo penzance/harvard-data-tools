338bf65a2811227c527ee7335e6db475adbe9d58
Phil McGachey <phil_mcgachey@harvard.edu>
2017-06-13 14:04:48 -0400

    Adding outline of mediasites extensions

---
+++ java/mediasites_data_client/src/main/java/edu/harvard/data/mediasites/MediasitesPhase0Bootstrap.java
@@ -0,0 +1,61 @@
+package edu.harvard.data.mediasites;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.model.S3ObjectId;
+
+import edu.harvard.data.AwsUtils;
+import edu.harvard.data.DataConfigurationException;
+import edu.harvard.data.pipeline.Phase0Bootstrap;
+import edu.harvard.data.schema.UnexpectedApiResponseException;
+
+public class MediasitesPhase0Bootstrap extends Phase0Bootstrap
+implements RequestHandler<BootstrapParameters, String> {
+
+  @Override
+  public String handleRequest(final BootstrapParameters params, final Context context) {
+    try {
+      super.init(params.getConfigPathString(), MediasitesDataConfig.class, true);
+      System.out.println("The message was " + params.getMessage());
+      super.run(context);
+    } catch (IOException | DataConfigurationException | UnexpectedApiResponseException e) {
+      return "Error: " + e.getMessage();
+    }
+    return "";
+  }
+
+  @Override
+  protected List<S3ObjectId> getInfrastructureConfigPaths() {
+    final List<S3ObjectId> paths = new ArrayList<S3ObjectId>();
+    final S3ObjectId configPath = AwsUtils.key(config.getCodeBucket(), "infrastructure");
+    paths.add(AwsUtils.key(configPath, "tiny_phase_0.properties"));
+    paths.add(AwsUtils.key(configPath, "tiny_emr.properties"));
+    return paths;
+  }
+
+  @Override
+  protected Map<String, String> getCustomEc2Environment() {
+    final Map<String, String> env = new HashMap<String, String>();
+    env.put("DATA_SET_ID", runId);
+    env.put("DATA_SCHEMA_VERSION", "1.0");
+    return env;
+  }
+
+  @Override
+  protected boolean newDataAvailable()
+      throws IOException, DataConfigurationException, UnexpectedApiResponseException {
+    return true;
+  }
+
+  @Override
+  protected void setup()
+      throws IOException, DataConfigurationException, UnexpectedApiResponseException {
+  }
+
+}
---
+++ java/mediasites_data_tools/src/main/java/edu/harvard/data/matterhorn/MediasitesCodeManager.java
@@ -0,0 +1,74 @@
+package edu.harvard.data.matterhorn;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import edu.harvard.data.CodeManager;
+import edu.harvard.data.DataConfig;
+import edu.harvard.data.DataConfigurationException;
+import edu.harvard.data.HadoopJob;
+import edu.harvard.data.Phase0;
+import edu.harvard.data.identity.HadoopIdentityKey;
+import edu.harvard.data.identity.IdentityScrubber;
+import edu.harvard.data.mediasites.MediasitesDataConfig;
+import edu.harvard.data.mediasites.identity.MediasitesIdentityHadoopManager;
+
+public class MediasitesCodeManager extends CodeManager {
+
+  private final MediasitesIdentityHadoopManager identity;
+
+  public MediasitesCodeManager() {
+    this.identity = new MediasitesIdentityHadoopManager();
+  }
+
+  @Override
+  public Map<String, Class<? extends Mapper<Object, Text, ?, HadoopIdentityKey>>> getIdentityMapperClasses() {
+    return identity.getMapperClasses();
+  }
+
+  @Override
+  public Map<String, Class<? extends IdentityScrubber<?>>> getIdentityScrubberClasses() {
+    return identity.getScrubberClasses();
+  }
+
+  @Override
+  public List<String> getIdentityTableNames() {
+    return identity.getIdentityTableNames();
+  }
+
+  @Override
+  public Class<?> getIdentityPreverifyJob() {
+    return null;
+  }
+
+  @Override
+  public Class<?> getIdentityPostverifyJob() {
+    return null;
+  }
+
+  @Override
+  public Map<Integer, List<Class<? extends HadoopJob>>> getHadoopProcessingJobs() {
+    return jobs;
+  }
+
+  @Override
+  public DataConfig getDataConfig(final String configPathString, final boolean verify)
+      throws IOException, DataConfigurationException {
+    return MediasitesDataConfig.parseInputFiles(MediasitesDataConfig.class, configPathString,
+        verify);
+  }
+
+  @Override
+  public Phase0 getPhase0(final String configPathString, final String datasetId, final String runId,
+      final ExecutorService exec) throws IOException, DataConfigurationException {
+    final MediasitesDataConfig config = MediasitesDataConfig
+        .parseInputFiles(MediasitesDataConfig.class, configPathString, false);
+    return new MediasitesPhase0(config, runId, exec);
+  }
+
+}
