7d672ef35b6a14b7be9437fb1aae77b8f672bd04
SdotGlenn <sonnyg.lopez@gmail.com>
2018-06-14 14:53:00 -0400

    Enable schema version 3.0.1
    * Minor chagnes to descriptions. Change parenthese to brackets for 'deprecated' vars

--- java/canvas_data_tools/src/main/java/edu/harvard/data/canvas/CanvasPhase0.java
+++ java/canvas_data_tools/src/main/java/edu/harvard/data/canvas/CanvasPhase0.java
@@ -188,6 +188,7 @@ public class CanvasPhase0 extends Phase0 {
 
   private void checkSchema() throws VerificationException {
     final Set<String> validSchemas = new HashSet<String>();
+    validSchemas.add("3.0.1");
     validSchemas.add("3.0.0");
     validSchemas.add("2.0.0");
     validSchemas.add("1.18.0");
