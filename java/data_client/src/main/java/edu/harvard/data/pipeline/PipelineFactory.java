f8b9f6177d3e60cebd58d01c11af7d8a3caff9c8
SdotGlenn <sonnyg.lopez@gmail.com>
2018-08-01 12:37:13 -0400

    Full Text Incremental Updates for Partial Tables
    * Remove ebs optimize

--- java/data_client/src/main/java/edu/harvard/data/pipeline/PipelineFactory.java
+++ java/data_client/src/main/java/edu/harvard/data/pipeline/PipelineFactory.java
@@ -335,7 +335,6 @@ public class PipelineFactory {
   
   public PipelineObjectBase testMasterEbsConfig() {
 	  final PipelineObjectBase obj = new PipelineObjectBase(config, "EBSConfiguration", "EbsConfiguration"); 
-	  obj.set("ebsOptimized", "true");
 	  obj.set("ebsBlockDeviceConfig", testEbsBlockDeviceConfig());
 	  allObjects.add(obj);	  
 	  return obj;
