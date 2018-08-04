c14a915fa9f1bd5a4019208853a476500b715b9a
SdotGlenn <sonnyg.lopez@gmail.com>
2018-08-03 20:01:39 -0400

    Added Canvas REST Extension
    * Set record separator as \r\n for testing

--- java/data_client/src/main/java/edu/harvard/data/FormatLibrary.java
+++ java/data_client/src/main/java/edu/harvard/data/FormatLibrary.java
@@ -98,7 +98,7 @@ public class FormatLibrary {
       .withNullString("\\N").withRecordSeparator("\n").withIgnoreSurroundingSpaces(false);
 
   private static final CSVFormat INTERNAL_CSV_FORMAT = CSVFormat.TDF.withQuote('"').withEscape('/')
-      .withNullString("\\N").withRecordSeparator("\n").withIgnoreSurroundingSpaces(false);
+      .withNullString("\\N").withRecordSeparator("\r\n").withIgnoreSurroundingSpaces(false);
 
   private static final CSVFormat INTERNAL_SIS_FORMAT = CSVFormat.TDF.withQuote('"').withEscape('/')
 	      .withNullString("null").withRecordSeparator("\n").withIgnoreSurroundingSpaces(false);  
