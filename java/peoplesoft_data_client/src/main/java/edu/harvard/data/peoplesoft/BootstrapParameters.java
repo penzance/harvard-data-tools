fb7c21d5518440a466452ecd207ac7dc8df2e1ff
SdotGlenn <glenn_lopez@harvard.edu>
2019-09-30 20:45:37 -0400

    Rapid Pipeline Dynamic Requests (Peoplesoft)
    * Peoplesoft BootstrapParameters: Add rapid config dict.
    * Peoplesoft Codegenerator: Add rapidrequsts config
    * Peoplesoft Phase0Bootstrap: Get input from lambda input stream to capture entire json request.

--- java/peoplesoft_data_client/src/main/java/edu/harvard/data/peoplesoft/BootstrapParameters.java
+++ java/peoplesoft_data_client/src/main/java/edu/harvard/data/peoplesoft/BootstrapParameters.java
@@ -1,38 +1,97 @@
 package edu.harvard.data.peoplesoft;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class BootstrapParameters {
-  private String configPathString;
-  private boolean downloadOnly;
-  private String message;
+	  private String configPathString;
+	  private boolean downloadOnly;
+	  private Map<String,String> rapidConfigDict;
+	  private String message;
 
-  public String getConfigPathString() {
-    return configPathString;
-  }
+	  public String getConfigPathString() {
+	    return configPathString;
+	  }
 
-  public void setConfigPathString(final String configPathString) {
-    this.configPathString = configPathString;
-  }
+	  public String getConfigPathStringKeyValue() {
+		final String key = "\"configPathString\":\"";
+		final String value = getConfigPathString();
+		final String enclose = "\"";
+	    return key.concat(value).concat(enclose);
+	  }
 
-  public boolean getDownloadOnly() {
-    return downloadOnly;
-  }
+	  public void setConfigPathString(final String configPathString) {
+	    this.configPathString = configPathString;
+	  }
 
-  public void setDownloadOnly(final boolean downloadOnly) {
-    this.downloadOnly = downloadOnly;
-  }
+	  public boolean getDownloadOnly() {
+	    return downloadOnly;
+	  }
 
-  public String getMessage() {
-    return message;
-  }
+	  public void setDownloadOnly(final boolean downloadOnly) {
+	    this.downloadOnly = downloadOnly;
+	  }
+	  
+	  public void setRapidDictString( final Map<String,String> rapidConfigDict) {
+		 this.rapidConfigDict = rapidConfigDict;
+	  }
+	  
+	  public String getRapidConfigDictString() {
+		  
+		final String key = "\"rapidConfigDict\":";
+		final String value = mapToString(getRapidConfigDict());
+	    return key.concat(value);
+	  }
+	  
+	  public String mapToString(final Map<String, String> mapDict) {
+	        StringBuilder sb = new StringBuilder();
+	        Iterator<Entry<String, String>> iter = mapDict.entrySet().iterator();
+	        sb.append("{");
+	        while (iter.hasNext()) {
+	            Entry<String, String> entry = iter.next();
+	            sb.append('"');
+	            sb.append(entry.getKey());
+	            sb.append('"');
+	            sb.append(':').append('"');
+	            sb.append(entry.getValue());
+	            sb.append('"');
+	            if (iter.hasNext()) {
+	                sb.append(',').append(' ');
+	            }
+	        }
+	        sb.append("}");
+	        return sb.toString();
+	  }
+	  
+	  
+	  public Map<String,String> getRapidConfigDict() {
+		return rapidConfigDict;
+	  }
 
-  public void setMessage(final String message) {
-    this.message = message;
-  }
+	  public String getMessage() {
+	    return message;
+	  }
 
-  @Override
-  public String toString() {
-    return "BootstrapParams\n  ConfigPath: " + configPathString + "\n  message: "
-        + message + "\n  downloadOnly: " + downloadOnly;
-  }
+	  public void setMessage(final String message) {
+	    this.message = message;
+	  }
+
+	  @Override
+	  public String toString() {
+	    return "BootstrapParams\n  ConfigPath: " + configPathString + "\n  message: "
+			+ message + "\n  downloadOnly: " + downloadOnly + "\n  rapidConfigDict: "
+			+ rapidConfigDict.toString();
+	  }
+	  
+	  public String getRequestString() {
+		  final String start = "{";
+		  final String end= "}";
+		  final String comma= ",";
+		  final String total = getConfigPathStringKeyValue().concat(comma)
+				  											.concat(getRapidConfigDictString());
+		  final String all = start.concat(total).concat(end);
+		  return all;
+	  }
 
 }
+
