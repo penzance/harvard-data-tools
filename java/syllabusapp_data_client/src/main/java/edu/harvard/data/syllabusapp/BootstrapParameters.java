fbf9827e4662360595cde388974c6af2bd55d079
SdotGlenn <glenn_lopez@harvard.edu>
2019-12-19 12:19:09 -0500

    SyllabusApp Baseline
    * Python: Generate tools and Phase 0 scripts
    * Java: Created data_client and data_tools for syllabusapp
    * Schema: Created syllabusapp schema checks.

---
+++ java/syllabusapp_data_client/src/main/java/edu/harvard/data/syllabusapp/BootstrapParameters.java
@@ -0,0 +1,38 @@
+package edu.harvard.data.syllabusapp;
+
+public class BootstrapParameters {
+  private String configPathString;
+  private boolean createPipeline=false;
+  private String message;
+
+  public String getConfigPathString() {
+    return configPathString;
+  }
+
+  public void setConfigPathString(final String configPathString) {
+    this.configPathString = configPathString;
+  }
+
+  public boolean getCreatePipeline() {
+    return createPipeline;
+  }
+
+  public void setCreatePipeline(final boolean createPipeline) {
+    this.createPipeline = createPipeline;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(final String message) {
+    this.message = message;
+  }
+
+  @Override
+  public String toString() {
+    return "BootstrapParams\n  ConfigPath: " + configPathString + "\n  message: "
+        + message + "\n  createPipeline: " + createPipeline;
+  }
+
+}
