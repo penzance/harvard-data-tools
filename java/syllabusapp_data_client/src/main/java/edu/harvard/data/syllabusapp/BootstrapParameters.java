eb9d007d5446b6b19e07c3d09930434bd5401db8
SdotGlenn <glenn_lopez@harvard.edu>
2020-01-13 10:20:49 -0500

    SyllabusApp Baseline
    * Bootstrap: Added RAPID Config params.

--- java/syllabusapp_data_client/src/main/java/edu/harvard/data/syllabusapp/BootstrapParameters.java
+++ java/syllabusapp_data_client/src/main/java/edu/harvard/data/syllabusapp/BootstrapParameters.java
@@ -1,38 +1,100 @@
 package edu.harvard.data.syllabusapp;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class BootstrapParameters {
-  private String configPathString;
-  private boolean createPipeline=false;
-  private String message;
+	  private String configPathString;
+	  private boolean createPipeline=true;
+	  private Map<String,String> rapidConfigDict;
+	  private String message;
 
-  public String getConfigPathString() {
-    return configPathString;
-  }
+	  public String getConfigPathString() {
+	    return configPathString;
+	  }
 
-  public void setConfigPathString(final String configPathString) {
-    this.configPathString = configPathString;
-  }
+	  public String getConfigPathStringKeyValue() {
+		final String key = "\"configPathString\":\"";
+		final String value = getConfigPathString();
+		final String enclose = "\"";
+	    return key.concat(value).concat(enclose);
+	  }
 
-  public boolean getCreatePipeline() {
-    return createPipeline;
-  }
+	  public void setConfigPathString(final String configPathString) {
+	    this.configPathString = configPathString;
+	  }
 
-  public void setCreatePipeline(final boolean createPipeline) {
-    this.createPipeline = createPipeline;
-  }
+	  public boolean getCreatePipeline() {
+		return createPipeline;
+	  }
 
-  public String getMessage() {
-    return message;
-  }
+	  public void setCreatePipeline(final boolean createPipeline) {
+		this.createPipeline = createPipeline;
+	  }
+	  
+	  public void setRapidDictString( final Map<String,String> rapidConfigDict) {
+		 this.rapidConfigDict = rapidConfigDict;
+	  }
+	  
+	  public String getRapidConfigDictString() {
+		try {
+		  final String key = "\"rapidConfigDict\":";
+		  final String value = mapToString(getRapidConfigDict());
+	      return key.concat(value);
+	    } catch (NullPointerException e) {
+	      return "null";
+	    }
+	  }
+	  
+	  public String mapToString(final Map<String, String> mapDict) {
+	        StringBuilder sb = new StringBuilder();
+	        Iterator<Entry<String, String>> iter = mapDict.entrySet().iterator();
+	        sb.append("{");
+	        while (iter.hasNext()) {
+	            Entry<String, String> entry = iter.next();
+	            sb.append('"');
+	            sb.append(entry.getKey());
+	            sb.append('"');
+	            sb.append(':').append('"');
+	            sb.append(entry.getValue());
+	            sb.append('"');
+	            if (iter.hasNext()) {
+	                sb.append(',').append(' ');
+	            }
+	        }
+	        sb.append("}");
+	        return sb.toString();
+	  }
+	  
+	  
+	  public Map<String,String> getRapidConfigDict() {
+		return rapidConfigDict;
+	  }
 
-  public void setMessage(final String message) {
-    this.message = message;
-  }
+	  public String getMessage() {
+	    return message;
+	  }
 
-  @Override
-  public String toString() {
-    return "BootstrapParams\n  ConfigPath: " + configPathString + "\n  message: "
-        + message + "\n  createPipeline: " + createPipeline;
-  }
+	  public void setMessage(final String message) {
+	    this.message = message;
+	  }
+
+
+	  @Override
+	  public String toString() {
+	    return "BootstrapParams\n  ConfigPath: " + getConfigPathString()
+		+ "\n  createPipeline: " + getCreatePipeline()
+		+ "\n  rapidConfigDict: " + getRapidConfigDictString();
+	  }
+
+	  public String getRequestString() {
+		  final String start = "{";
+		  final String end= "}";
+		  final String comma= ",";
+		  final String total = getConfigPathStringKeyValue().concat(comma)
+				  											.concat(getRapidConfigDictString());
+		  final String all = start.concat(total).concat(end);
+		  return all;
+	  }
 
 }
