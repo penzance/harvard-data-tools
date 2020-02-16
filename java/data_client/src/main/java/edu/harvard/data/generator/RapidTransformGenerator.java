d8cfe6ba12174b035fc790e6f9419768f2acf079
SdotGlenn <glenn_lopez@harvard.edu>
2020-02-15 21:18:14 -0500

    Rapid Pipeline Dynamic Requests
    * rapidTransformGenerator: redirect and append both stdout and stderr to file (get python logger output).

--- java/data_client/src/main/java/edu/harvard/data/generator/RapidTransformGenerator.java
+++ java/data_client/src/main/java/edu/harvard/data/generator/RapidTransformGenerator.java
@@ -157,10 +157,10 @@ public class RapidTransformGenerator {
 		  out.println();
 		  out.println("#Run RAPID Code\n"
 				    + "cd $RAPID_CODE_BASE\n"
-				    + "python runtime/main.py --metadata --runtime >> /var/log/rapid-transform.out\n"
-				    + "python runtime/main.py --data-requests --runtime >> /var/log/rapid-transform.out\n"
-				    + "python runtime/main.py --alert --runtime >> /var/log/rapid-transform.out\n"
-				    + "python testing/main.py --test --data-requests --env staging >> /var/log/rapid-transform.out\n");
+				    + "python runtime/main.py --metadata --runtime >> /var/log/rapid-transform.out 2>&1\n"
+				    + "python runtime/main.py --data-requests --runtime >> /var/log/rapid-transform.out 2>&1\n"
+				    + "python runtime/main.py --alert --runtime >> /var/log/rapid-transform.out 2>&1\n"
+				    + "python testing/main.py --test --data-requests --env staging >> /var/log/rapid-transform.out 2>&1\n");
 		  out.println();
 		  
 	  }
